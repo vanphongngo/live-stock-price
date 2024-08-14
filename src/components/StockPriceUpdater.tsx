42af3eacc41e5208e850ee92d1ea3070d6f25492
Phong Ngo <phongvanngo.nvp@gmail.com>
2024-08-14 20:29:10 +0700

    update live stock price

--- src/components/StockPriceUpdater.tsx
+++ src/components/StockPriceUpdater.tsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
 const StockPriceUpdater: React.FC = () => {
-    const [stockPrice, setStockPrice] = useState<number | null>(null);
+    const [stockPrice, setStockPrice] = useState<string | null>(null);
 
     useEffect(() => {
         const eventSource = new EventSource('https://api.uat.finpal.com.vn/api/v1/real-time/stock/VIH');
 
         eventSource.onmessage = (event) => {
-            const data = JSON.parse(event.data);
-            setStockPrice(data.stockPrice);
+            setStockPrice(event.data);
         };
 
         return () => {
@@ -17,7 +16,7 @@ const StockPriceUpdater: React.FC = () => {
     }, []);
 
     return (
-        <div>
+        <div style={{width: '500px', wordWrap: 'break-word' }}>
             <h2>Stock Price Updater</h2>
             {stockPrice !== null ? (
                 <p>Stock Price: {stockPrice}</p>
