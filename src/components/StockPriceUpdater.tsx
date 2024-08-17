0f022e3725d1493e3d10c775cb2c72b3068dbdb9
Phong Ngo <phongvanngo.nvp@gmail.com>
2024-08-17 10:41:34 +0700

    update live stock price

--- src/components/StockPriceUpdater.tsx
+++ src/components/StockPriceUpdater.tsx
@@ -1,34 +1,61 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 const StockPriceUpdater: React.FC = () => {
     const [stockPrice, setStockPrice] = useState<string | null>(null);
     const [ticker, setTicker] = useState<string | null>(null);
+    const [baseUrl, setBaseUrl] = useState<string>(``);
     const inputRef = useRef<HTMLInputElement>(null);
+    const inputBaseUrlRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
+        if (localStorage.getItem("baseUrl")) {
+            setBaseUrl(localStorage.getItem("baseUrl") as string);
+        } else {
+            setBaseUrl('https://api.uat.finpal.com.vn/api/v1/real-time/stock/');
+        }
+    }, []);
+
+    useEffect(() => {
+
+        if (inputBaseUrlRef.current) {
+            inputBaseUrlRef.current.value = baseUrl;
+        }
+
         if (!ticker) return;
 
-        const eventSource = new EventSource(`https://api.uat.finpal.com.vn/api/v1/real-time/stock/${ticker}`);
+        const eventSource = new EventSource(`${baseUrl}${ticker}`);
 
         eventSource.onmessage = (event) => {
+            console.log('Received message', event.data);
             setStockPrice(event.data);
         };
 
         return () => {
             eventSource.close();
         };
-    }, [ticker]);
+    }, [ticker, baseUrl]);
 
     const handleSubscribe = () => {
         if (inputRef.current && inputRef.current.value) {
             setTicker(inputRef.current.value);
         }
     };
-
+    const handleChangeBaseUrl = () => {
+        if (inputBaseUrlRef.current && inputBaseUrlRef.current.value) {
+            localStorage.setItem('baseUrl', inputBaseUrlRef.current.value);
+            setBaseUrl(inputBaseUrlRef.current.value);
+        }
+    };
     return (
-        <div style={{ width: '500px', wordWrap: 'break-word' }}>
+        <div style={{width: '700px', wordWrap: 'break-word', alignSelf: 'left'}}>
+            <br/>
+            <span>baseUrl</span>
+            <input style={{width:'400px'}} ref={inputBaseUrlRef}/>
+            <button onClick={handleChangeBaseUrl}>changeBaseUrl</button>
+            <br/>
+            <br/>
             <span>Ticker: </span>
-            <input ref={inputRef} />
+            <input ref={inputRef}/>
             <button onClick={handleSubscribe}>Subscribe</button>
             <h2>Stock Price Updater</h2>
             {stockPrice !== null ? (
